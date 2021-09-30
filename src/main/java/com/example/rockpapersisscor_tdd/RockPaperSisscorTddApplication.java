1dd972fe564f02a3dc962218931b984c93584c4a
FilipStaaf <69985953+FilipStaaf@users.noreply.github.com>
2021-09-30 10:19:06 +0200

    Start commit of project. Rosita, Patrik, Robin, Filip

---
+++ src/main/java/com/example/rockpapersisscor_tdd/RockPaperSisscorTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.rockpapersisscor_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperSisscorTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RockPaperSisscorTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rockpapersisscor_tdd/RockPaperSisscorTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rockpapersisscor_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperSisscorTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
