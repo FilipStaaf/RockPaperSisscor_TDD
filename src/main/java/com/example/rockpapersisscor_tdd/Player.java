3e7874466f2ea14bbebfb0e585455ce95ef26512
Robin Axelstorm <69907783+Robinax@users.noreply.github.com>
2021-10-07 11:16:51 +0200

    made some refactoring. Filip,Rosita,Robin,Patrik

--- src/main/java/com/example/rockpapersisscor_tdd/Player.java
+++ src/main/java/com/example/rockpapersisscor_tdd/Player.java
@@ -3,9 +3,6 @@ package com.example.rockpapersisscor_tdd;
 import com.example.rockpapersisscor_tdd.service.Move;
 import lombok.Value;
 
-import java.util.Locale;
-import java.util.Scanner;
-
 @Value
 public class Player {
     private String name;
@@ -14,29 +11,5 @@ public class Player {
     public Player(String name, Move move) {
         this.name = name;
         this.move = move;
-    }
-
-    static String getName(Scanner input) {
-        System.out.println("Enter your name: ");
-        String name = input.nextLine();
-        if (name.length() < 1) {
-            name = "";
-        }
-        return name;
-    }
-
-    //get the moves
-    public Move getMove(Scanner input) {
-        System.out.println("Enter your move: ");
-        String stringMove = input.nextLine().toUpperCase(Locale.ROOT);
-        if (stringMove.equals("ROCK")) return Move.ROCK;
-        if (stringMove.equals("SCISSOR")) {
-            return Move.SCISSOR;
-        }
-        if (stringMove.equals("PAPER")) {
-            return Move.PAPER;
-        } else {
-            return null;
-        }
     }
 }
